9910d1a715539fe1289c4d40770a9b4c489a81cf
Ruben-PC\Ruben <rubengonzalezperez1@gmail.com>
2019-10-20 19:20:29 +0200

    Puebas select

---
+++ selectPrueba.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$dbname = "blog";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT USU.NOMB FROM usuarios";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "bai";
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($conn);
+?>
