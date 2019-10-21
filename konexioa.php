5bd01dacc6a994fed071e9ec19b0b9249c2e5c4c
Gorka Intxausti <ik_2dw3@H015110.NET>
2019-10-21 13:16:45 +0200

    gorka

--- konexioa.php
+++ konexioa.php
@@ -9,7 +9,6 @@ $usuario = "root";
 $nombre_base_de_datos = "blog";
 try{
 	$konexioa = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
-	echo "BIEN";
 }catch(Exception $e){
 	echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
